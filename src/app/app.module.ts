77b07b949f6b8b4ad7ac109ee87816eeadbe3201
Erlend Eindride Fasmer <eefasmer@gmail.com>
2017-06-27 21:30:54 +0200

    egen komponent for hovedbrett.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainBoardComponent } from './components/main-board/main-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBoardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/main-board/main-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-board',
+  templateUrl: './main-board.component.html',
+  styleUrls: ['./main-board.component.css']
+})
+export class MainBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
