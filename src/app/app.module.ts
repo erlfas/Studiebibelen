f698ee10d11a62078525be2ad9f08683542c2e04
Erlend Eindride Fasmer <eefasmer@gmail.com>
2017-07-03 21:08:11 +0200

    Et skjelett jeg er fornøyd med.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { MainBoardComponent } from './components/main-board/main-board.component';
 import { VelgKommentarComponent } from './components/velg-kommentar/velg-kommentar.component';
-import { TestDropdownComponent } from './components/test-dropdown/test-dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainBoardComponent,
-    VelgKommentarComponent,
-    TestDropdownComponent
+    VelgKommentarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    NgbModule.forRoot()
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
